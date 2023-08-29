4e0bdc2b887c4d9948f8c6154f5bfb40d08ed45c
Celeron-PC <a@a.comc>
2023-08-29 14:40:33 +0500

    Какой-то коммит

--- lib/widgets/app.dart
+++ lib/widgets/app.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/widgets/test.dart';
 import 'package:flutter_application_1/widgets/user_profile.dart';
 
 class App extends StatelessWidget {
@@ -10,9 +11,9 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-          colorSchemeSeed: Color.fromARGB(255, 28, 10, 76),
+          colorSchemeSeed: Color.fromARGB(255, 255, 255, 255),
           useMaterial3: false),
-      home: UserProfile(),
+      home: LayoutTest(),
     );
   }
 }
